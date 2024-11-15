644954e20f8c62db91b3ed5087d8f1160fceef0f
Martina Garcia <martinagg@myuax.com>
2024-11-15 12:33:42 +0100

    intento conexion templates

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-html_base = """
-<h1>Mi Web Personal</h1>
-<ul>
-<li><a href="/">Portada</a></li>
-<li><a href="/about/">Acerca de</a></li>
-<li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
+
 def home(request):
     return render(request, "core/home.html")
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Me llamo Martina y me encanta Django!</p>
-    """)
+    return render(request,"core/about.html")
+def portfolio(request):
+    return render(request,"core/portfolio.html")
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contacto</h2>
-    <p>Aquí os dejo mi email y mis redes sociales:</p>
-    <ul>
-    <li><a href="mailto:hola@martina.net">Email</a></li>
-    <li><a href="https://github.com/martinagg7">Github</a></li>
-    <li><a href="https://youtube.com">Youtube</a></li>
-    </ul>
-    """)
+    return render(request,"core/contact.html")
