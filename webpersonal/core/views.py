c663e2d260c5707a500b5841c9c00b0a83cd8d12
Martina Garcia <martinagg@myuax.com>
2024-11-15 12:08:14 +0100

    vistas y urls

---
+++ webpersonal/core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+html_base = """
+<h1>Mi Web Personal</h1>
+<ul>
+<li><a href="/">Portada</a></li>
+<li><a href="/about/">Acerca de</a></li>
+<li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
+def home(request):
+    return HttpResponse(html_base + """
+    <h2>Bienvenidos</h2>
+    <p>Esto es la portada.</p>
+    """)
+def about(request):
+    return HttpResponse(html_base + """
+    <h2>Acerca de</h2>
+    <p>Me llamo Martina y me encanta Django!</p>
+    """)
+def contact(request):
+    return HttpResponse(html_base + """
+    <h2>Contacto</h2>
+    <p>Aquí os dejo mi email y mis redes sociales:</p>
+    <ul>
+    <li><a href="mailto:hola@martina.net">Email</a></li>
+    <li><a href="https://github.com/martinagg7">Github</a></li>
+    <li><a href="https://youtube.com">Youtube</a></li>
+    </ul>
+    """)
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path("admin/", admin.site.urls),
 ]
