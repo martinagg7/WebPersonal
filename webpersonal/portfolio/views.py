3880d2311c0e9c0bd60481ef9ad94b6972870cfa
Martina Garcia <martinagg@myuax.com>
2024-11-16 17:08:40 +0100

    prueba guardar post

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Project  #para recuperar lo que guardamos
 
 # Create your views here.
 
 def portfolio(request):
+    projects=Project.objects.all() #para recuperar todos los projectos
     return render(request,"portfolio/portfolio.html")
