00991426178e178dc9eda2b07ac988b9bd7593b0
Martina Garcia <martinagg@myuax.com>
2024-11-16 16:33:41 +0100

    cargamos media

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-# Create your models here.
+
 class Project(models.Model):
-    title=models.CharField(max_length=250)
+
+    title = models.CharField(max_length=250)
     description = models.TextField()
     image = models.ImageField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        #para ordenarlos seg se crean
-        ordering=["-created"]
+        ordering = ["-created"]
+
     def __str__(self):
         return self.title
