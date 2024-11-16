79185e11b8a7c8c3cd8b82f17ef75a43c557b687
Martina Garcia <martinagg@myuax.com>
2024-11-16 17:16:46 +0100

    casi fiin

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -4,5 +4,5 @@ from .models import Project  #para recuperar lo que guardamos
 # Create your views here.
 
 def portfolio(request):
-    projects=Project.objects.all() #para recuperar todos los projectos
-    return render(request,"portfolio/portfolio.html")
+    projects=Project.objects.all() #para recuperar todos los projectos le pasamos el diccionario
+    return render(request,"portfolio/portfolio.html",{'projects':projects})
